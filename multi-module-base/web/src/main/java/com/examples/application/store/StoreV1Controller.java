aeefcf26e3ab1520ab64c06f08f9f1b735ee404b
Kalman Kostenszky <k.kostenszky@gmail.com>
2025-12-09 15:06:30 +0100

    upgrade multi-module-base to Spring Boot 4
    * use Spring Boot 4 modularized dependencies
    * remove unused imports

--- multi-module-base/web/src/main/java/com/examples/application/store/StoreV1Controller.java
+++ multi-module-base/web/src/main/java/com/examples/application/store/StoreV1Controller.java
@@ -4,16 +4,11 @@ import java.util.Map;
 
 import com.examples.application.api.v1.OrderDto;
 import com.examples.application.api.v1.StoreApi;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 class StoreV1Controller implements StoreApi {
-
 
 	@Override
 	public ResponseEntity<Void> deleteOrder(Long orderId) throws Exception {
